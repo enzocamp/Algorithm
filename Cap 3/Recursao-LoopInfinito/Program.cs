b3aa55f79b053b069f0387e601348c00e6b3f884
Enzo Camp <enzo.flap@hotmail.com>
2025-02-10 16:23:17 -0300

    Selection Sort

---
+++ Cap 3/Recursao-LoopInfinito/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        static void Regressiva(int number)
+        {
+            Console.WriteLine(number);
+
+            if (number < 1) return; // Aqui é o caso-base
+
+            Regressiva(number - 1); // Aqui é caso-recursivo
+        }
+
+        Regressiva(10);
+    }
+}
