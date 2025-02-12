14b31ff672d8c932feec76a37cf6daf414e21902
Enzo Camp <enzo.flap@hotmail.com>
2025-02-12 12:46:55 -0300

    Recursion CallStack

---
+++ Cap 3/CallStack-Fatorial/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int fat(int x)
+        {
+            if (x == 1) return 1;
+
+            else return x * fat(x - 1);
+        }
+
+        fat(3);
+
+    }
+}
